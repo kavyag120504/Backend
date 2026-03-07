d371aca6b4fbe68434811b80148c103b5ba0d8b2
kavya1205 <kavya.goswami.23cse@bmu.edu.in>
2026-03-08 01:00:02 +0530

    index file updated

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+const app = express();
+
+dotenv.config({
+    path:"./public/.env"
+});
+
+connectDB();
